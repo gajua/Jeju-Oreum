e9508b7c9c585664e1d975a382f25fef15853ef5
sewon kim <sewon0325@naver.com>
2022-11-21 00:10:35 +0900

    모달 위지, 정보 불러오기

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,6 @@
-async function loadData(){
-  const response = await fetch('https://api.odcloud.kr/api/15096996/v1/uddi:6738a90c-ec96-4245-a187-9528cea62904?page=1&perPage=90&serviceKey=3MCBWEYPV4%2BY4Un8XqdBpFBiaGQKGsEVpC1HIK1DCoHqjNlhaUGcwjBIJGDYeTaTOiG4GKJorKXpGpfNpOEjhQ%3D%3D')
-  const data = await response.json()
-  return data
-}
-
+const modal = document.querySelector(".modal");
+const modalText = document.querySelector(".text");
+const oreumNameList = document.querySelector(".oreum-name");
 class OreumList {
   constructor() {
     this.oreumList = document.querySelector(".list-oreum");
@@ -27,9 +24,9 @@ class OreumList {
 
   jejuOreum(data) {
     const docFrag = document.createDocumentFragment("li");
-    
+
     const jeju = { lat: 33.3616658, lng: 126.5204118 };
-    const map = new google.maps.Map(document.getElementById("map"), {
+    let map = new google.maps.Map(document.getElementById("map"), {
       zoom: 11,
       center: jeju,
     });
@@ -41,40 +38,50 @@ class OreumList {
       let 설명 = pos[i].설명;
       let 위도 = Number(pos[i].위도);
       let 경도 = Number(pos[i].경도);
-      let marker = new google.maps.Marker({
-        position: {
-          lat: 위도,
-          lng: 경도,
-        },
-        map: map,
-      });
 
+      const li = document.createElement("li");
+      li.innerHTML = `<p class='oreum-name'>${오름명}</p>`;
+      li.data;
 
-      const li = document.createElement('li')
-      li.innerHTML=`<p>${오름명}</p>`
-      li.data
+      this.oreumList.appendChild(li);
+      li.addEventListener("click", info);
 
-      this.oreumList.appendChild(li)
+      function info(e) {
+        modal.classList.toggle("none");
+        modalText.innerHTML = `${오름명} : ${설명}`;
+        const 위치 = { lat: 위도, lng: 경도 };
+        map = new google.maps.Map(document.getElementById("map"), {
+          zoom: 12,
+          center: 위치,
+        });
+        const test = pos.find(
+          (el) => el.오름명 === this.firstChild.textContent
+        );
 
+        let marker = new google.maps.Marker({
+          position: {
+            lat: Number(test.위도),
+            lng: Number(test.경도),
+          },
+          map: map,
+        });
 
-      function info (e){
-        const modal = document.querySelector('.modal')
-        modal.classList.toggle('none')
+        marker.addListener("click", () => {
+          map.panTo(marker.position);
+        });
+
+        marker.addListener("mouseover", function () {
+          infowindow.open(map, marker);
+          infowindow.setContent(오름명);
+        });
+
+        marker.addListener("mouseout", function () {
+          infowindow.close();
+        });
+
+        // this.firstChild
       }
-      li.addEventListener('click',info)
-      marker.addListener("click", () => {
-        map.panTo(marker.position);
-        console.log(오름명+': ' +설명);
-      });
-      marker.addListener('mouseover', function() {
-        infowindow.open(map, marker);
-        infowindow.setContent(오름명)
-    });
-      marker.addListener('mouseout', function() {
-        infowindow.close();
-    });
     }
-
   }
 }
 
